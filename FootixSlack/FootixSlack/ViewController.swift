fc2f0322e33f66eaf8defa51654a6c0721e91f85
Adrien CARANTA <adrien.caranta@gmail.com>
2016-03-12 19:37:52 -0500

    Initial project setup

---
+++ FootixSlack/FootixSlack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FootixSlack
+//
+//  Created by Adrien CARANTA on 2016-03-12.
+//  Copyright © 2016 Footix. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
